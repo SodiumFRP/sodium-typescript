3b1d812911177a2a100a0b342a86a71b1fbfe4a0
David Komer <david.komer@gmail.com>
2018-01-15 10:44:31 +0200

    don't minify npm bundles

--- rollup.config.umd.js
+++ rollup.config.umd.js
@@ -1,16 +1,19 @@
 import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json';
 import replace from 'rollup-plugin-replace';
-//import uglify from 'rollup-plugin-uglify';
-//import { minify } from 'uglify-es';
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-es';
 
 export default [
 	{
         input: './src/lib/Sodium.ts',
         external: [ 'typescript-collections', 'sanctuary-type-classes' ],
         output: [
-          { file: pkg.module, format: 'es', sourcemap: true },
-          { file: pkg.main, format: 'cjs', sourcemap: true },
+          { file: "dist/sodium.umd.min.js", name: "Sodium", format: 'umd', sourcemap: true,
+            globals: {
+              'typescript-collections': 'Collections',
+              'sanctuary-type-classes': 'Z',
+            }
+          },
         ],
         plugins: [
             replace({
@@ -19,7 +22,7 @@ export default [
 
             typescript(),
 
-            //uglify({}, minify)
+            uglify({}, minify)
         ]
 	}
 ];
