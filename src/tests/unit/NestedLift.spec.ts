fd2737ce466981c3c53aa73cd4278afa34c7eb0a
David Komer <david.komer@gmail.com>
2018-01-25 15:48:17 +0200

    updated test

--- src/tests/unit/NestedLift.spec.ts
+++ src/tests/unit/NestedLift.spec.ts
@@ -10,21 +10,21 @@ import {
     Cell,
     CellLoop,
     getTotalRegistrations
-  } from '../../lib/Lib';
-  
-  afterEach(() => {
+} from '../../lib/Lib';
+
+afterEach(() => {
     if (getTotalRegistrations() != 0) {
-      throw new Error('listeners were not deregistered');
+        throw new Error('listeners were not deregistered');
     }
-  });
-  
-  test('nested lift', (done) => {
+});
+
+test('nested lift', (done) => {
     const out = new Array<number>();
     const ccOriginal = new Cell<Cell<number>>(new Cell(1));
     const sOffset = new StreamSink<number>();
     const cOffset = sOffset.hold(0);
 
-    const cTotal = ccOriginal.map(cOriginal => 
+    const cTotal = ccOriginal.map(cOriginal =>
         cOriginal.lift(cOffset, (value, offset) => value + offset)
     );
 
@@ -33,7 +33,7 @@ import {
     const kill = cFinal.listen(value => {
         out.push(value);
 
-        if(out.length === 2) {
+        if (out.length === 2) {
             done();
         }
     });
@@ -43,4 +43,37 @@ import {
     kill();
 
     expect(out).toEqual([1, 3, 5]);
-  });
+});
+
+
+test('lift + nested data/map', (done) => {
+    interface Data {
+        cValue: Cell<number>;
+    }
+    const out = new Array<number>();
+    const cValue = new Cell(1);
+    const cOriginal = new Cell<Data>({cValue: cValue});
+    const sOffset = new StreamSink<number>();
+    const cOffset = sOffset.hold(0);
+    
+    const cTotal = cOriginal.lift(cOffset, (data, offset) => {
+        return data.cValue.map(value => value + offset)
+    })
+    
+    const cFinal = Cell.switchC(cTotal);
+    
+    const kill = cFinal.listen(value => {
+        out.push(value);
+
+        if (out.length === 2) {
+            done();
+        } 
+    });
+
+    sOffset.send(2);
+    sOffset.send(4);
+    kill();
+
+    expect(out).toEqual([1, 3, 5]);
+
+});
