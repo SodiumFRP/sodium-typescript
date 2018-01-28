750e910917de6f1a307ae74f1c27130a4e8cd17a
dakom <david.komer@gmail.com>
2018-01-28 07:28:38 +0200

    unit test

--- src/tests/unit/NestedLift.spec.ts
+++ src/tests/unit/NestedLift.spec.ts
@@ -54,21 +54,21 @@ test('lift + nested data/map', (done) => {
     const cOriginal = new Cell<Data>({cValue: new Cell(1)});
     const sOffset = new StreamSink<number>();
     const cOffset = sOffset.hold(0);
-    
+
     const cTotal = cOriginal.lift(cOffset, (data, offset) => {
         return {
             cValue: data.cValue.map(value => value + offset)
         }
     })
-    
+
     const cFinal = Cell.switchC(cTotal.map(data => data.cValue));
-    
+
     const kill = cFinal.listen(value => {
         out.push(value);
 
         if (out.length === 2) {
             done();
-        } 
+        }
     });
 
     sOffset.send(2);
@@ -78,3 +78,34 @@ test('lift + nested data/map', (done) => {
     expect(out).toEqual([1, 3, 5]);
 });
 
+test('map + nested data/lift', (done) => {
+  interface Data {
+    cValue: Cell<number>;
+  }
+  const out = new Array<number>();
+  const cOriginal = new Cell<Data>({cValue: new Cell(1)});
+  const sOffset = new StreamSink<number>();
+  const cOffset = sOffset.hold(0);
+
+  const cTotal = cOriginal.map((data:Data) => {
+    return {
+      cValue: data.cValue.lift(cOffset, (value, offset) => value + offset)
+    }
+  })
+
+  const cFinal = Cell.switchC(cTotal.map(data => data.cValue));
+
+  const kill = cFinal.listen(value => {
+    out.push(value);
+    if (out.length === 1) { //only one firing happens this time
+      done();
+    }
+  });
+
+  sOffset.send(2);
+  sOffset.send(4);
+
+  kill();
+  //expect(out).toEqual([1, 3, 5]); - this fails! See https://github.com/SodiumFRP/sodium-typescript/wiki/Changes-in-nested-Cells
+  expect(out).toEqual([1]);
+});
