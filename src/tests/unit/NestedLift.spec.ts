3ce5cb529993abed1a907f2740f91ae290c1f7dd
David Komer <david.komer@gmail.com>
2018-02-09 13:16:06 +0200

    consolidated test

--- src/tests/unit/NestedLift.spec.ts
+++ src/tests/unit/NestedLift.spec.ts
@@ -2,7 +2,6 @@
 import {
     lambda1,
     StreamSink,
-    StreamLoop,
     CellSink,
     Transaction,
     Tuple2,
@@ -148,4 +147,159 @@ test('map + nested data/lift (no Transaction)', (done) => {
 
     expect(out).toEqual([1, 3, 5]);
 
+});
+
+test('example 2: with loop', (done) => {
+
+  //Pick one...
+
+  //General cleanup / final checking helpers
+  const results = []
+  const expected = [
+    "BAZ",  
+    "",
+    "apple",
+    "apple",
+    "APPLE",
+    "",
+    "",
+    ""
+  ]
+  const unlisteners = [];
+  const finish = () => setTimeout(() => { //postponed a frame for convenience
+    unlisteners.forEach(fn => fn());
+    expect(results.length).toBe(expected.length);
+    results.forEach((v, i) => expect(v).toEqual(expected[i]));
+    done();
+  }, 0);
+
+  const sWrite = new StreamSink<boolean>();
+
+  //Modify items
+  const sModify = new StreamSink<string>();
+  const makeUppercase = (target: string, s: string) => target === s ? s.toUpperCase() : s;
+
+  //Manage list of items
+  const sAdd = new StreamSink<string>();
+  const sRemove = new StreamSink<string>();
+  const sRemoveAll = new StreamSink<string>();
+
+  const cItems = Transaction.run(() => {
+    const makeItem = (label: string): Cell<string> => {
+      const cLoop = new CellLoop<string>();
+      const cUpdate = sModify.snapshot(cLoop, makeUppercase).hold(label);
+
+      cLoop.loop(cUpdate);
+
+      return cLoop;
+    };
+
+    const addItem = (label: string) => (c: Cell<string>) => makeItem(label);
+    const removeAll = () => (c: Cell<string>) => new Cell("");
+
+    const applyToList = (fn, xs) => fn(xs);
+
+    const emptyCell = new Cell("");
+
+    const ccLoop = new CellLoop<Cell<string>>();
+    const ccUpdate =
+      sAdd.map(addItem)
+        .orElse(sRemoveAll.map(removeAll))
+        .snapshot(ccLoop, applyToList)
+        .hold(emptyCell);
+
+    ccLoop.loop(ccUpdate);
+    const ccItems = ccLoop;
+
+    const cResult = Cell.switchC(ccItems); //Then switchC on it to get Cell<Array<string>>
+    
+    //None of these helps!
+    unlisteners.push(emptyCell.listen(() => {}));
+    unlisteners.push(ccUpdate.listen(() => {}));
+    unlisteners.push(ccItems.listen(() => {}));
+    unlisteners.push(ccLoop.listen(() => {}));
+    unlisteners.push(cResult.listen(() => {}));
+
+    return cResult;
+  });
+
+  //Flush writes
+  unlisteners.push(
+    sWrite.snapshot(cItems, (evt, items) => {
+      results.push(items);
+      return evt;
+    })
+      .listen(evt => {
+        if (evt) {
+          finish();
+        }
+      })
+  );
+
+  //This is just for the sake of debugging
+  unlisteners.push(
+    cItems.listen(items => {
+      //console.log(items);
+    })
+  );
+
+  //expected state (after write): "BAZ"
+  sAdd.send("foo");
+  sAdd.send("bar");
+  sAdd.send("baz");
+  sModify.send("baz");
+  sWrite.send(false);
+
+  //expected state: []
+  sRemoveAll.send(null);
+  sWrite.send(false);
+
+  //expected state: ["apple"]
+  sAdd.send("apple");
+  sWrite.send(false);
+
+  //expected state: ["apple"]
+  sModify.send("foo");
+  sWrite.send(false)
+
+  //expected state: ["APPLE"]
+  sModify.send("apple");
+  sWrite.send(false);
+
+  //expected state: []
+  sRemoveAll.send(null);
+  sWrite.send(false);
+
+  //expected state: []
+  //But first - if there's nothing in the list, sModify needs a dummy listener
+  unlisteners.push(sModify.listen(() => { }));
+  sModify.send("foo");
+  sWrite.send(false);
+
+  
+  expect(cItems.sample()).toEqual(""); //Just to confirm here, it really is a Cell of []
+  //-------HERE'S WHERE IT GETS WEIRD!!! -------------
+  //The dummy listener which was added in this outer scope is no longer valid if we add+clear again
+
+  //Specifically, adding both of these two lines:
+  sAdd.send("foo");
+  sRemoveAll.send(null);
+
+  //WITHOUT adding this line:
+  unlisteners.push(sModify.listen(() => { }));
+
+  //Causes a "send() was invoked before listeners were registered" here:
+  sModify.send("foo");
+
+  /*
+    Note that either of these fixes it:
+    1. Commending out either the sAdd or sRemoveAll
+    2. Uncommenting the re-adding of the listener
+  */
+
+
+  //----------DONE------------------------
+  //expected state : []
+  sRemoveAll.send(null);
+  sWrite.send(true);
 });
