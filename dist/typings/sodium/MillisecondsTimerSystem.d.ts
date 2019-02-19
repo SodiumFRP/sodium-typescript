ed1c665de0d4420a4c99b2decc3b04ba7284d0f0
Clinton Selke <clinuxrulz@gmail.com>
2019-02-19 20:22:36 +1000

    dist added to .gitignore (again), and prepublish script

--- dist/typings/sodium/MillisecondsTimerSystem.d.ts
+++
@@ -1,7 +0,0 @@
-import { TimerSystem } from "./TimerSystem";
-/**
- * A timer system implementation using milliseconds as the time unit.
- */
-export declare class MillisecondsTimerSystem extends TimerSystem {
-    constructor();
-}
