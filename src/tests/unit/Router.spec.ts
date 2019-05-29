77627c210f918f6b568514066a695c5d09fe8bcf
Clinton Selke <clinuxrulz@gmail.com>
2019-05-29 10:14:08 +1000

    removed unneeded listeners from test case

--- src/tests/unit/Router.spec.ts
+++ src/tests/unit/Router.spec.ts
@@ -19,14 +19,11 @@ test('should test Router', (done) => {
     let sc = router.filterIncludes(2).mapTo("b");
     let sd = router.filterIncludes(3).mapTo("c");
     let kill = sb.merge(sc, (x,y) => x+y).merge(sd, (x,y) => x+y).listen(x => out.push(x));
-    let kills = [sb.listen(() => {}), sc.listen(() => {}), sd.listen(() => {})];
-    let kill2 = () => kills.forEach(x => x());
     sa.send([1]);
     sa.send([2]);
     sa.send([3]);
     sa.send([1,2,3]);
     kill();
-    kill2();
     expect(out).toEqual(["a", "b", "c", "abc"]);
     done();
 });
